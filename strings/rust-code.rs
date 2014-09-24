d26570889c917713c9bbe9c2a488e3f6c4df81f5
Robert Sedlacek <rs@474.at>
2014-09-24 22:58:43 +0200

    capitalization

--- strings/rust-code.rs
+++ strings/rust-code.rs
@@ -8,21 +8,21 @@ use std::collections::{ HashMap };
 
 fn process_string(text: &str) -> String {
 
-    // create a HashMap to collect character counts into
+    // Create a HashMap to collect character counts into
     let mut map = HashMap::new();
 
-    // increase count in HashMap for each character
+    // Increase count in HashMap for each character
     for c in text.chars() {
         map.insert_or_update_with(c, 1u, |_, v| *v += 1);
     }
 
-    // collect the keys in a Vec so we can sort them
+    // Collect the keys in a Vec so we can sort them
     let mut keys = map.keys().map(|k| *k).collect::<Vec<char>>();
 
-    // sort by occurrence as tracked in the HashMap
+    // Sort by occurrence as tracked in the HashMap
     keys.sort_by(|a, b| map[*b].cmp(&map[*a]));
     
-    // return new String with ordered charrs
+    // Return new String with ordered charrs
     String::from_chars(keys.as_slice())
 }
 
@@ -30,27 +30,27 @@ fn process_string(text: &str) -> String {
 pub unsafe extern "C"
 fn chars_by_count(inp: *const c_char) -> *const c_char {
 
-    // this is where we'll store the pointer for the output string
+    // This is where we'll store the pointer for the output string
     let mut ptr = 0 as *const c_char;
 
-    // we're using features requiring the runtime (HashMap/TaskRng), so we'll run
+    // We're using features requiring the runtime (HashMap/TaskRng), so we'll run
     // our transform in an isolated task
     let task = native::task::new((0, std::uint::MAX));
     task.run(|| {
 
-        // wrap the C string pointer in something we can handle
-        // we can't own the pointer, or there will be a segfault
-        // (I assume FFI::Raw wants to keep owning it)
+        // Wrap the C string pointer in something we can handle.
+        // We can't own the pointer, or there will be a segfault
+        // (I assume FFI::Raw wants to keep owning it).
         let inp_cstring = CString::new(inp, false);
 
-        // calculate our sorted string into another C string
+        // Calculate our sorted string into another C string
         let out_cstring = process_string(inp_cstring.as_str().unwrap()).to_c_str();
 
-        // get the pointer for our result string
+        // Get the pointer for our result string
         ptr = out_cstring.unwrap();
 
     }).drop();
 
-    // return the pointer to the string
+    // Return the pointer to the string
     ptr
 }
